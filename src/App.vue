77518bd7fbad2a98dd105660498f6466aed1d3e3
Asjas <asjas@outlook.com>
2019-11-18 22:21:51 +0200

    Add Header, Main and Form Vue Components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Header />
+    <Main />
+    <Form />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Form from "./components/Form.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    Header,
+    Main,
+    Form,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+:root {
+  --cyan: hsl(180, 66%, 49%);
+  --read: hsl(0, 87%, 67%);
+  --gray: hsl(0, 0%, 75%);
+  --grayish-violet: hsl(257, 7%, 63%);
+  --dark-violet: hsl(257, 27%, 26%);
+  --very-dark-blue: hsl(255, 11%, 22%);
+  --very-dark-violet: hsl(260, 8%, 14%);
+}
+html,
+body {
+  height: 100%;
+  font-family: "Poppins", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  margin: 0;
+  font-size: 18px;
+}
+.sr-only {
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: auto;
+  margin: 0;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+  white-space: nowrap;
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,45 @@
+<template>
+  <form class="form">
+    <input class="form__input" type="text" placeholder="Shorten a link here..." />
+    <button class="form__button">Shorten It!</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Form extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.form {
+  display: grid;
+  background: var(--dark-violet);
+  padding: 20px 20px;
+  margin: 60px 20px 80px 20px;
+  border-radius: 10px;
+  gap: 15px;
+}
+.form__input {
+  color: var(--gray);
+  height: 38px;
+  font-size: 14px;
+  padding-left: 14px;
+  border: 0;
+  border-radius: 6px;
+  width: calc(100% - 14px);
+  font-family: "Poppins", sans-serif;
+}
+.form__button {
+  font-size: 16px;
+  color: white;
+  background: var(--cyan);
+  border: 0;
+  border-radius: 6px;
+  width: 100%;
+  height: 42px;
+  font-family: "Poppins", sans-serif;
+}
+</style>
