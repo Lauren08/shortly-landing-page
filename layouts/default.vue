df8bc4dce4bb8a8bd3d94a11d99a853df06d6ec1
Asjas <asjas@outlook.com>
2020-02-19 16:44:33 +0200

    Add ::selection that uses main theme color

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,6 +23,16 @@ html {
   margin: 0;
 }
 
+::-moz-selection {
+  background: #2acfcf;
+  color: #ffffff;
+}
+
+::selection {
+  background: #2acfcf;
+  color: #ffffff;
+}
+
 .btn {
   @apply mx-auto h-14 w-48 border-0 rounded-full outline-none bg-cyan text-white font-bold;
 }
