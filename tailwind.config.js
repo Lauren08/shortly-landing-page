ea57a015d86b73ed885f38f8e46fd703c42cb539
A-J Roos <asjas@outlook.com>
2020-03-02 22:48:06 +0200

    Refactor TailwindCSS theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 module.exports = {
   theme: {
-    colors: {
-      cyan: "#2ACFCF",
-      darkvoilet: "#3B3054",
-      white: "#FFFFFF",
-      secondary: {
-        red: "#F46262"
+    extend: {
+      colors: {
+        cyan: "#2ACFCF",
+        darkvoilet: "#3B3054",
+        secondary: {
+          red: "#F46262"
+        },
+        neutral: {
+          gray: "#BFBFBF",
+          lightgray: "#EFF1F7",
+          grayishvoilet: "#9E9AA7",
+          verydarkblue: "#35323E",
+          verydarkvoilet: "#232127"
+        }
       },
-      neutral: {
-        gray: "#BFBFBF",
-        lightgray: "#EFF1F7",
-        grayishvoilet: "#9E9AA7",
-        verydarkblue: "#35323E",
-        verydarkvoilet: "#232127"
+      spacing: {},
+      borderWidth: {
+        "3": "3px"
       }
     },
     fontFamily: {
@@ -21,8 +26,6 @@ module.exports = {
     },
     screens: {
       sm: "375px",
-      md: "768px",
-      lg: "1024px",
       xl: "1440px"
     },
     fontSize: {
@@ -41,35 +44,6 @@ module.exports = {
       "2xl": "1.66rem",
       "3xl": "2rem",
       "4xl": "2.66rem"
-    },
-    borderWidth: {
-      default: "1px",
-      "0": "0",
-      "2": "2px",
-      "3": "3px"
-    },
-    spacing: {
-      1: "4px",
-      2: "10px",
-      3: "12px",
-      4: "16px",
-      5: "20px",
-      6: "24px",
-      8: "32px",
-      10: "40px",
-      12: "48px",
-      14: "54px",
-      16: "64px",
-      20: "76px",
-      24: "96px",
-      32: "128px",
-      40: "160px",
-      48: "192px",
-      56: "280px",
-      60: "300px",
-      64: "327px"
     }
-  },
-  variants: {},
-  plugins: []
+  }
 };
