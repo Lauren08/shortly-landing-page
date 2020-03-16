e65a5ac708da433c9c5ae50004362180cc5befec
A-J Roos <asjas@outlook.com>
2020-03-16 20:06:51 +0200

    Adjust HTML markup used in Desktop Navigation

--- components/DesktopNav.vue
+++ components/DesktopNav.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="hidden md:flex md:w-10/12">
-    <ul class="list-none flex w-full justify-between items-center">
-      <ul class="flex w-full">
+    <div class="flex w-full justify-between items-center">
+      <ul class="flex w-full list-none">
         <li>
           <a
             class="px-4 no-underline focus:outline-none focus:shadow-outline text-neutral-grayishvoilet hover:text-neutral-verydarkblue text-tiny font-bold"
@@ -21,16 +21,18 @@
           >Resources</a>
         </li>
       </ul>
-      <li>
-        <a
-          class="mr-2 px-4 no-underline focus:outline-none focus:shadow-outline text-neutral-grayishvoilet hover:text-neutral-verydarkblue text-tiny font-bold"
-          href="#"
-        >Login</a>
-      </li>
-      <li>
-        <button class="btn full-nav-btn">Sign Up</button>
-      </li>
-    </ul>
+      <ul class="flex list-none">
+        <li>
+          <a
+            class="mr-2 px-4 no-underline focus:outline-none focus:shadow-outline text-neutral-grayishvoilet hover:text-neutral-verydarkblue text-tiny font-bold"
+            href="#"
+          >Login</a>
+        </li>
+        <li>
+          <button class="btn full-nav-btn">Sign Up</button>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
