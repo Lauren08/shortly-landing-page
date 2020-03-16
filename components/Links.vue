df51cc705fc620c907847b890739fc661bf73f31
A-J Roos <asjas@outlook.com>
2020-03-16 21:00:44 +0200

    replace `section` with `div` container

--- components/Links.vue
+++ components/Links.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="flex flex-col items-center w-full">
+  <div class="flex flex-col items-center w-full">
     <div class="w-full" v-for="link in links" :key="link.created_at">
       <Link :link="link" />
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
