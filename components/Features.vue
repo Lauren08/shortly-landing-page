58f0ed978bffb28eaeae278e2266fa74718bdc02
A-J Roos <asjas@outlook.com>
2020-03-16 20:41:31 +0200

    Remove w-full class from features content

--- components/Features.vue
+++ components/Features.vue
@@ -2,12 +2,10 @@
   <section class="pt-20">
     <div class="w-full md:w-3/5 lg:w-2/5 mx-auto">
       <h2
-        class="w-11/12 lg:w-full mx-auto text-neutral-verydarkblue text-2xl lg:text-4xl leading-2xl lg:leading-4xl tracking-tighter font-bold text-center"
-      >
-        Advanced Statistics
-      </h2>
+        class="w-11/12 mx-auto text-neutral-verydarkblue text-2xl lg:text-4xl leading-2xl lg:leading-4xl tracking-tighter font-bold text-center"
+      >Advanced Statistics</h2>
       <p
-        class="w-11/12 lg:w-full mt-2 pt-2 mx-auto px-1 text-neutral-grayishvoilet text-small lg:text-base lg:leading-2xl leading-2xl tracking-wide text-center font-medium"
+        class="w-11/12 mt-2 pt-2 mx-auto px-1 text-neutral-grayishvoilet text-small lg:text-base lg:leading-2xl leading-2xl tracking-wide text-center font-medium"
       >
         Track how your links are performing across the web with our advanced
         statistics dashboard.
