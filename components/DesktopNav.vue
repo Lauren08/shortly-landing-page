1a94bf83ee950bd259193d087a04557b4cf52cd0
A-J Roos <asjas@outlook.com>
2020-03-16 19:56:40 +0200

    Fix markup used in Desktop navigation

--- components/DesktopNav.vue
+++ components/DesktopNav.vue
@@ -1,35 +1,31 @@
 <template>
   <nav class="hidden md:flex md:w-10/12">
     <ul class="list-none flex w-full justify-between items-center">
-      <div class="flex w-full">
+      <ul class="flex w-full">
         <li>
           <a
             class="px-4 no-underline focus:outline-none focus:shadow-outline text-neutral-grayishvoilet hover:text-neutral-verydarkblue text-tiny font-bold"
             href="#"
-            >Features</a
-          >
+          >Features</a>
         </li>
         <li>
           <a
             class="px-4 no-underline focus:outline-none focus:shadow-outline text-neutral-grayishvoilet hover:text-neutral-verydarkblue text-tiny font-bold"
             href="#"
-            >Pricing</a
-          >
+          >Pricing</a>
         </li>
         <li>
           <a
             class="px-4 no-underline focus:outline-none focus:shadow-outline text-neutral-grayishvoilet hover:text-neutral-verydarkblue text-tiny font-bold"
             href="#"
-            >Resources</a
-          >
+          >Resources</a>
         </li>
-      </div>
+      </ul>
       <li>
         <a
           class="mr-2 px-4 no-underline focus:outline-none focus:shadow-outline text-neutral-grayishvoilet hover:text-neutral-verydarkblue text-tiny font-bold"
           href="#"
-          >Login</a
-        >
+        >Login</a>
       </li>
       <li>
         <button class="btn full-nav-btn">Sign Up</button>
